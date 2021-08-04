e7aaf863cd24fb79740797f4dbc5d2bf8c03ce6c
meri21 <milesgh07@gmail.com>
2021-08-03 21:44:38 -0400

    Optimization changes

--- freadr.c
+++ freadr.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
-#include <string.h>
-main(){
-    FILE *f;
-    char path[100];
-    int c;
-    printf("Enter the filepath: \n");
-    fgets(path, 100, stdin);
-    path[strlen (path) - 1] = '\0';
-    f = fopen(path, "r");
-    c = fgetc(f);
-    while (1) {
-        if (feof(f)){
-	    goto eof;
-        }
-        fputc(c, stdout);
-        c = fgetc(f);
-    }
-    eof:
-        fclose(f);
+
+void show(FILE* f){
+    char c;
+    while ((c = getc(f)) != EOF) putc(c, stdout);
 }
+
+main(int argc, char** argv){
+    FILE* file;
+    if (argc == 1){
+        printf("freadr: no file given \n");
+        return 1;
+    }
+    file = fopen(argv[1], "r");
+    show(file);
+    fclose(file);
+}
+
